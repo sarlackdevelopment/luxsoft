d38205ee57ac57e1b599dec765b05a8586e37bb0
Korobov Alexandr <askorobov@vtb.ru>
2020-08-31 09:38:47 +0300

    add freemarker

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -1,37 +1,16 @@
 package org.example.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import org.example.domain.Messages;
-import org.example.repos.MessagesRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import java.util.Map;
 
-import java.util.List;
-
-@RestController
-@RequestMapping("message")
+@Controller
 public class MainController {
 
-    private final MessagesRepo messagesRepo;
-
-    @Autowired
-    public MainController(MessagesRepo messagesRepo) {
-        this.messagesRepo = messagesRepo;
+    @GetMapping("/")
+    public String startPage(Map<String, Object> model) {
+        return "start";
     }
-
-    @GetMapping
-    public List<Messages> Example() {
-        return messagesRepo.findAll();
-    }
-
-    @PostMapping
-    public Messages create(@RequestBody Messages message) {
-        return messagesRepo.save(message);
-    }
-
 
 }
